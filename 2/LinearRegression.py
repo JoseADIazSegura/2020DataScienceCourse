55435ac68024dd641d7b7c10fce3af974190dc7c
Jose Armando Diaz Segura <jdiazseg9611@universidadean.edu.co>
2020-07-11 15:23:07 -0500

    Initial commit

---
+++ 2/LinearRegression.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from pandas import DataFrame
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("../dataSrc/3.2 cost_revenue_clean.csv")
+
+print(data.describe())
+
+productionBudget = DataFrame(data, columns=['production_budget_usd'])
+revenue = DataFrame(data, columns=['worldwide_gross_usd'])
+
+plt.figure(figsize=(10,6))
+plt.scatter(productionBudget,revenue,alpha=0.3)
+plt.title('Film Cost vs Global Revenue')
+plt.xlabel('Production Budget ')
+plt.ylabel('Worldwide Gross $')
+plt.ylim(0,3000000000)
+plt.xlim(0,450000000)
+plt.xlim()
+plt.show()
+
